e200fde2c58fe6c200f8b6b1afe93ce224a55c7e
Jason Dellaluce <jasondellaluce@gmail.com>
2022-01-17 13:18:04 +0000

    update(dummy): bump plugin version to 0.2.0
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- plugins/dummy/dummy.go
+++ plugins/dummy/dummy.go
@@ -39,7 +39,7 @@ const (
 	PluginName                      = "dummy"
 	PluginDescription               = "Reference plugin for educational purposes"
 	PluginContact                   = "github.com/falcosecurity/plugins"
-	PluginVersion                   = "0.1.0"
+	PluginVersion                   = "0.2.0"
 	PluginEventSource               = "dummy"
 )
 
