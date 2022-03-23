10cd992540f183a7a9ab5c39029838ac4c3a383f
Jason Dellaluce <jasondellaluce@gmail.com>
2022-03-23 18:45:40 +0000

    fix(plugins/json): support json pointer escapes
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- plugins/json/json.go
+++ plugins/json/json.go
@@ -123,10 +123,17 @@ func (m *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
 		if arg[0] == '/' {
 			arg = arg[1:]
 		}
-		hc := strings.Split(arg, "/")
-		val := m.jdata.Get(hc...)
-		if val == nil {
-			return fmt.Errorf("json key not found: %s", arg)
+
+		// walk the object using the json pointer syntax (RFC 6901)
+		pointer := strings.Split(arg, "/")
+		val := m.jdata
+		for _, key := range pointer {
+			key = strings.Replace(key, "~1", "/", -1)
+			key = strings.Replace(key, "~0", "~", -1)
+			val = val.Get(key)
+			if val == nil {
+				return nil
+			}
 		}
 
 		if val.Type() == fastjson.TypeString {
