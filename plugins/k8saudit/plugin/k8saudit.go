91cc17b436a982362aed594bde0354b225b9eae5
Jason Dellaluce <jasondellaluce@gmail.com>
2022-05-31 10:15:10 +0000

    refactor(plugins/k8saudit): bump sdk go version
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- plugins/k8saudit/plugin/k8saudit.go
+++ plugins/k8saudit/plugin/k8saudit.go
@@ -17,15 +17,19 @@ limitations under the License.
 package main
 
 import (
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/extractor"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
 	"github.com/falcosecurity/plugins/plugins/k8saudit/pkg/k8saudit"
 )
 
 func init() {
-	p := &k8saudit.Plugin{}
-	source.Register(p)
-	extractor.Register(p)
+	plugins.SetFactory(func() plugins.Plugin {
+		p := &k8saudit.Plugin{}
+		source.Register(p)
+		extractor.Register(p)
+		return p
+	})
 }
 
 func main() {}
