eab23107f727b343e9374528c6cbf85bf5fe796c
Jason Dellaluce <jasondellaluce@gmail.com>
2022-06-21 14:16:55 +0000

    update(plugins/json): add optional init configuration
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

---
+++ plugins/json/pkg/json/config.go
@@ -0,0 +1,26 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package json
+
+type PluginConfig struct {
+	UseAsync bool `json:"useAsync" jsonschema:"description=If true then async extraction optimization is enabled (Default: true)"`
+}
+
+// Resets sets the configuration to its default values
+func (k *PluginConfig) Reset() {
+	k.UseAsync = true
+}
