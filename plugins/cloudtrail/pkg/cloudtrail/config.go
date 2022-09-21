3032fd1fb815a8a0aac192ab5adf3b4cddc3c016
Jason Dellaluce <jasondellaluce@gmail.com>
2022-09-21 12:55:47 +0000

    update(plugins/cloudtrail): add aws client config overrides
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- plugins/cloudtrail/pkg/cloudtrail/config.go
+++ plugins/cloudtrail/pkg/cloudtrail/config.go
@@ -22,6 +22,11 @@ type PluginConfig struct {
 	SQSDelete             bool `json:"sqsDelete" jsonschema:"title=Delete SQS messages,description=If true then the plugin will delete SQS messages from the queue immediately after receiving them (Default: true),default=true"`
 	UseAsync              bool `json:"useAsync" jsonschema:"title=Use async extraction,description=If true then async extraction optimization is enabled (Default: true),default=true"`
 	UseS3SNS              bool `json:"useS3SNS" jsonschema:"title=Use S3 SNS,description=If true then the plugin will expect SNS messages to originate from S3 instead of directly from Cloudtrail (Default: false),default=false"`
+	AWS                   struct {
+		Config      string `json:"config" jsonschema:"title=Shared AWS Config File,description=If non-empty, overrides the AWS shared config filepath (e.g. ~/.aws/config), and has precedence over user-defined env variables such as AWS_CONFIG_FILE (Default: empty),default=''"`
+		Credentials string `json:"credentials" jsonschema:"title=Shared AWS Credentials File,description=If non-empty, overrides the AWS shared credentials filepath (e.g. ~/.aws/credentials), and has precedence over user-defined env variables such as AWS_SHARED_CREDENTIALS_FILE (Default: empty),default=''"`
+		Profile     string `json:"profile" jsonschema:"title=Shared AWS Config Profile,description=If non-empty, overrides the AWS shared config profile (e.g. 'default'), and has precedence over user-defined env variables such as AWS_PROFILE (Default: empty),default=''"`
+	} `json:"aws"`
 }
 
 // Reset sets the configuration to its default values
@@ -30,4 +35,7 @@ func (p *PluginConfig) Reset() {
 	p.S3DownloadConcurrency = 1
 	p.UseAsync = true
 	p.UseS3SNS = false
+	p.AWS.Config = ""
+	p.AWS.Credentials = ""
+	p.AWS.Profile = ""
 }
