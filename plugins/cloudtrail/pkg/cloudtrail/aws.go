1db426496417f74997c0be45f9f0933a65bbed83
Gerald Combs <gerald@zing.org>
2022-09-29 13:43:43 -0700

    update(plugins/cloudtrail): Add a region setting.
    Add a "region" configuration option which lets the user specify an AWS
    region.
    
    The jsonschema module was truncating descriptions with commas, so remove
    them for now.
    
    Signed-off-by: Gerald Combs <gerald@zing.org>

--- plugins/cloudtrail/pkg/cloudtrail/aws.go
+++ plugins/cloudtrail/pkg/cloudtrail/aws.go
@@ -27,14 +27,16 @@ import (
 // This can be included in plugins' init configuration struct definition
 // to declare AWS-specific config fields
 type PluginConfigAWS struct {
-	Profile     string `json:"profile" jsonschema:"title=Shared AWS Config Profile,description=If non-empty, overrides the AWS shared config profile (e.g. 'default') and env variables such as AWS_PROFILE (Default: empty),default=''"`
-	Config      string `json:"config" jsonschema:"title=Shared AWS Config File,description=If non-empty, overrides the AWS shared config filepath (e.g. ~/.aws/config) and env variables such as AWS_CONFIG_FILE (Default: empty),default=''"`
-	Credentials string `json:"credentials" jsonschema:"title=Shared AWS Credentials File,description=If non-empty, overrides the AWS shared credentials filepath (e.g. ~/.aws/credentials) and env variables such as AWS_SHARED_CREDENTIALS_FILE (Default: empty),default=''"`
+	Profile     string `json:"profile" jsonschema:"title=AWS Profile,description=If non-empty overrides the AWS shared configuration profile (e.g. 'default') and environment variables such as AWS_PROFILE (Default: empty),default="`
+	Region      string `json:"region" jsonschema:"title=AWS Region,description=If non-empty overrides the AWS region specified in the profile (e.g. 'us-east-1') and environment variables such as AWS_REGION (Default: empty),default="`
+	Config      string `json:"config" jsonschema:"title=Shared AWS Config File,description=If non-empty overrides the AWS shared configuration filepath (e.g. ~/.aws/config) and env variables such as AWS_CONFIG_FILE (Default: empty),default="`
+	Credentials string `json:"credentials" jsonschema:"title=Shared AWS Credentials File,description=If non-empty overrides the AWS shared credentials filepath (e.g. ~/.aws/credentials) and env variables such as AWS_SHARED_CREDENTIALS_FILE (Default: empty),default="`
 }
 
 // Reset sets the configuration to its default values
 func (p *PluginConfigAWS) Reset() {
 	p.Profile = ""
+	p.Region = ""
 	p.Config = ""
 	p.Credentials = ""
 }
@@ -46,6 +48,10 @@ func (p *PluginConfigAWS) ConfigAWS() (aws.Config, error) {
 
 	if len(p.Profile) > 0 {
 		opts = append(opts, config.WithSharedConfigProfile(p.Profile))
+	}
+
+	if len(p.Region) > 0 {
+		opts = append(opts, config.WithRegion(p.Region))
 	}
 
 	if len(p.Config) > 0 {
