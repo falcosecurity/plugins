0ada7d02ee33aed1d7214d227f603b90e516d54d
Aldo Lacuku <aldo@lacuku.eu>
2022-12-20 14:41:57 +0100

    refactor(build/registry): move index logic from main package to distribution package
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- build/registry/cmd/registry/registry.go
+++ build/registry/cmd/registry/registry.go
@@ -18,19 +18,13 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"github.com/falcosecurity/falcoctl/pkg/oci/authn"
 	"github.com/falcosecurity/plugins/build/registry/pkg/registry"
 	"github.com/falcosecurity/plugins/build/registry/pkg/registry/distribution"
 	"github.com/falcosecurity/plugins/build/registry/pkg/registry/oci"
 	"github.com/spf13/cobra"
 	"io/ioutil"
-	"oras.land/oras-go/v2/errdef"
-	"oras.land/oras-go/v2/registry/remote"
-	"oras.land/oras-go/v2/registry/remote/auth"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -84,77 +78,6 @@ func doTable(registryFile, subFile, subTag string) error {
 	return nil
 }
 
-func doUpdateIndex(registryFile, indexFile string) error {
-	var user, reg string
-	var found bool
-	if user, found = os.LookupEnv(oci.RegistryUser); !found {
-		return fmt.Errorf("environment variable with key %q not found, please set it before running this tool", oci.RegistryUser)
-	}
-
-	if reg, found = os.LookupEnv(oci.RegistryOCI); !found {
-		return fmt.Errorf("environment variable with key %q not found, please set it before running this tool", oci.RegistryOCI)
-	}
-
-	registryEntries, err := registry.LoadRegistryFromFile(registryFile)
-	if err != nil {
-		return err
-	}
-	ociEntries, err := ociRepos(registryEntries, reg, user)
-	if err != nil {
-		return err
-	}
-	if err := registryEntries.Validate(); err != nil {
-		return err
-	}
-
-	return distribution.UpsertIndex(registryEntries, ociEntries, indexFile)
-}
-
-func ociRepos(registryEntries *registry.Registry, reg, user string) (map[string]string, error) {
-	ociClient := authn.NewClient(auth.EmptyCredential)
-	ociEntries := make(map[string]string)
-
-	for _, entry := range registryEntries.Plugins {
-		if err := ociRepo(ociEntries, ociClient, oci.PluginNamespace, reg, user, entry.Name); err != nil {
-			return nil, err
-		}
-
-		if entry.RulesURL != "" {
-			if err := ociRepo(ociEntries, ociClient, oci.RulesfileNamespace, reg, user, entry.Name); err != nil {
-				return nil, err
-			}
-		}
-	}
-
-	return ociEntries, nil
-}
-
-func ociRepo(ociEntries map[string]string, client *auth.Client, ociRepoNamespace, reg, user, artifactName string) error {
-	ref := filepath.Join(reg, user, ociRepoNamespace, artifactName)
-
-	if ociRepoNamespace == oci.RulesfileNamespace {
-		artifactName = artifactName + distribution.RulesArtifactSuffix
-	}
-
-	repo, err := remote.NewRepository(ref)
-	if err != nil {
-		return fmt.Errorf("unable to create repo for ref %q: %w", ref, err)
-	}
-	repo.Client = client
-
-	_, _, err = repo.FetchReference(context.Background(), ref+":latest")
-	if err != nil && (errors.Is(err, errdef.ErrNotFound) || strings.Contains(err.Error(), "requested access to the resource is denied")) {
-		return nil
-	}
-
-	if err != nil {
-		return fmt.Errorf("unable to fetch reference for %q: %w", ref+":latest", err)
-	}
-
-	ociEntries[artifactName] = ref
-	return nil
-}
-
 func main() {
 	checkCmd := &cobra.Command{
 		Use:                   "check <filename>",
@@ -186,7 +109,7 @@ func main() {
 		Args:                  cobra.ExactArgs(2),
 		DisableFlagsInUseLine: true,
 		RunE: func(c *cobra.Command, args []string) error {
-			return doUpdateIndex(args[0], args[1])
+			return distribution.DoUpdateIndex(args[0], args[1])
 		},
 	}
 
