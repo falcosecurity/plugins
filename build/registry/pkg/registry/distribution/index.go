9beae0e2057853a3ddfe7bcc09a09e651e78d5ad
Aldo Lacuku <aldo@lacuku.eu>
2022-12-20 14:41:57 +0100

    refactor(build/registry): move index logic from main package to distribution package
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- build/registry/pkg/registry/distribution/index.go
+++ build/registry/pkg/registry/distribution/index.go
@@ -17,9 +17,18 @@ limitations under the License.
 package distribution
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/falcosecurity/falcoctl/pkg/index"
-	"github.com/falcosecurity/falcoctl/pkg/oci"
+	falcoctloci "github.com/falcosecurity/falcoctl/pkg/oci"
+	"github.com/falcosecurity/falcoctl/pkg/oci/authn"
 	"github.com/falcosecurity/plugins/build/registry/pkg/registry"
+	"github.com/falcosecurity/plugins/build/registry/pkg/registry/oci"
+	"oras.land/oras-go/v2/errdef"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -33,7 +42,7 @@ const (
 func pluginToIndexEntry(p registry.Plugin, registry, repo string) *index.Entry {
 	return &index.Entry{
 		Name:        p.Name,
-		Type:        string(oci.Plugin),
+		Type:        string(falcoctloci.Plugin),
 		Registry:    registry,
 		Repository:  repo,
 		Description: p.Description,
@@ -48,7 +57,7 @@ func pluginToIndexEntry(p registry.Plugin, registry, repo string) *index.Entry {
 func pluginRulesToIndexEntry(p registry.Plugin, registry, repo string) *index.Entry {
 	return &index.Entry{
 		Name:        p.Name + RulesArtifactSuffix,
-		Type:        string(oci.Rulesfile),
+		Type:        string(falcoctloci.Rulesfile),
 		Registry:    registry,
 		Repository:  repo,
 		Description: p.Description,
@@ -60,7 +69,7 @@ func pluginRulesToIndexEntry(p registry.Plugin, registry, repo string) *index.En
 	}
 }
 
-func UpsertIndex(r *registry.Registry, ociArtifacts map[string]string, indexPath string) error {
+func upsertIndex(r *registry.Registry, ociArtifacts map[string]string, indexPath string) error {
 	i := index.New(GHOrg)
 
 	if err := i.Read(indexPath); err != nil {
@@ -89,4 +98,75 @@ func UpsertIndex(r *registry.Registry, ociArtifacts map[string]string, indexPath
 	}
 
 	return i.Write(indexPath)
+}
+
+func DoUpdateIndex(registryFile, indexFile string) error {
+	var user, reg string
+	var found bool
+	if user, found = os.LookupEnv(oci.RegistryUser); !found {
+		return fmt.Errorf("environment variable with key %q not found, please set it before running this tool", oci.RegistryUser)
+	}
+
+	if reg, found = os.LookupEnv(oci.RegistryOCI); !found {
+		return fmt.Errorf("environment variable with key %q not found, please set it before running this tool", oci.RegistryOCI)
+	}
+
+	registryEntries, err := registry.LoadRegistryFromFile(registryFile)
+	if err != nil {
+		return err
+	}
+	ociEntries, err := ociRepos(registryEntries, reg, user)
+	if err != nil {
+		return err
+	}
+	if err := registryEntries.Validate(); err != nil {
+		return err
+	}
+
+	return upsertIndex(registryEntries, ociEntries, indexFile)
+}
+
+func ociRepos(registryEntries *registry.Registry, reg, user string) (map[string]string, error) {
+	ociClient := authn.NewClient(auth.EmptyCredential)
+	ociEntries := make(map[string]string)
+
+	for _, entry := range registryEntries.Plugins {
+		if err := ociRepo(ociEntries, ociClient, oci.PluginNamespace, reg, user, entry.Name); err != nil {
+			return nil, err
+		}
+
+		if entry.RulesURL != "" {
+			if err := ociRepo(ociEntries, ociClient, oci.RulesfileNamespace, reg, user, entry.Name); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return ociEntries, nil
+}
+
+func ociRepo(ociEntries map[string]string, client *auth.Client, ociRepoNamespace, reg, user, artifactName string) error {
+	ref := filepath.Join(reg, user, ociRepoNamespace, artifactName)
+
+	if ociRepoNamespace == oci.RulesfileNamespace {
+		artifactName = artifactName + RulesArtifactSuffix
+	}
+
+	repo, err := remote.NewRepository(ref)
+	if err != nil {
+		return fmt.Errorf("unable to create repo for ref %q: %w", ref, err)
+	}
+	repo.Client = client
+
+	_, _, err = repo.FetchReference(context.Background(), ref+":latest")
+	if err != nil && (errors.Is(err, errdef.ErrNotFound) || strings.Contains(err.Error(), "requested access to the resource is denied")) {
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("unable to fetch reference for %q: %w", ref+":latest", err)
+	}
+
+	ociEntries[artifactName] = ref
+	return nil
 }
