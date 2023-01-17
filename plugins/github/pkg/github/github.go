972cca0b3d50adfc8dfd4b6faa9a916fdca8fef4
Aldo Lacuku <aldo@lacuku.eu>
2023-01-17 10:24:22 +0100

    update(plugin/github): bump plugin version to v0.5.0
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- plugins/github/pkg/github/github.go
+++ plugins/github/pkg/github/github.go
@@ -38,7 +38,7 @@ const (
 	PluginName                = "github"
 	PluginDescription         = "Reads github webhook events, by listening on a socket or by reading events from disk"
 	PluginContact             = "github.com/falcosecurity/plugins"
-	PluginVersion             = "0.4.0"
+	PluginVersion             = "0.5.0"
 	PluginEventSource         = "github"
 	ExtractEventSource        = "github"
 )
