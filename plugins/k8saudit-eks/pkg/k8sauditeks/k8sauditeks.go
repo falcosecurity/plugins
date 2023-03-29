6e35f16b2ae0a39e55a07c55d2b4cf44ae3c56bf
Jason Dellaluce <jasondellaluce@gmail.com>
2023-03-29 17:26:52 +0000

    update(plugins): bump plugins versions
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- plugins/k8saudit-eks/pkg/k8sauditeks/k8sauditeks.go
+++ plugins/k8saudit-eks/pkg/k8sauditeks/k8sauditeks.go
@@ -57,7 +57,7 @@ func (k *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events for EKS from Cloudwatch Logs",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.1.0",
+		Version:     "0.1.1",
 		EventSource: "k8s_audit",
 	}
 }
