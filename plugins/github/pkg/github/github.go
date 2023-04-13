dc1e87e9195b6cdaad017ceb845d748e142588a7
Aldo Lacuku <aldo@lacuku.eu>
2023-04-13 10:07:08 +0200

    fix(plugins/github): fix ruleset dependencies
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- plugins/github/pkg/github/github.go
+++ plugins/github/pkg/github/github.go
@@ -38,7 +38,7 @@ const (
 	PluginName                = "github"
 	PluginDescription         = "Reads github webhook events, by listening on a socket or by reading events from disk"
 	PluginContact             = "github.com/falcosecurity/plugins"
-	PluginVersion             = "0.5.2"
+	PluginVersion             = "0.5.3"
 	PluginEventSource         = "github"
 	ExtractEventSource        = "github"
 )
