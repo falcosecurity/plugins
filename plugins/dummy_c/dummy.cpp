f23c702552bcd20357f692964149386136d66c3f
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-04-19 23:36:23 +0000

    update: dummy_c with new api
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- plugins/dummy_c/dummy.cpp
+++ plugins/dummy_c/dummy.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2022 The Falco Authors.
+Copyright (C) 2023 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,288 +14,144 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/* Reference "dummy" plugin, similar to the dummy plugin, but written
- * in C++. It uses the C++ sdk ../../sdk/cpp/falcosecurity_plugin.h
- * and implements classes that derive from
- * falcosecurity::source_plugin and falcosecurity::plugin_instance. */
-
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "dummy.h"
+#include "include/falcosecurity/internal/deps/plugin_types.h"
 #include "nlohmann/json.hpp"
-
-#include <falcosecurity_plugin.h>
 
 using json = nlohmann::json;
 
-class dummy_plugin : public falcosecurity::source_plugin {
-public:
-	dummy_plugin();
-	virtual ~dummy_plugin();
-
-	// All of these are from falcosecurity::source_plugin_iface.
-	void get_info(falcosecurity::plugin_info &info) override;
-	ss_plugin_rc init(const char *config) override;
-	void destroy() override;
-	falcosecurity::plugin_instance *create_instance(falcosecurity::source_plugin &plugin) override;
-	std::string event_to_string(const uint8_t *data, uint32_t datalen) override;
-	bool extract_str(const ss_plugin_event &evt, const std::string &field, const std::string &arg, std::string &extract_val) override;
-	bool extract_u64(const ss_plugin_event &evt, const std::string &field, const std::string &arg, uint64_t &extract_val) override;
-
-	// Return the configured jitter.
-	uint64_t jitter();
-
-private:
-	// A copy of the config provided to init()
-	std::string m_config;
-
-	// This reflects potential internal state for the plugin. In
-	// this case, the plugin is configured with a jitter (e.g. a
-	// random amount to add to the sample with each call to next().
-	uint64_t m_jitter;
-};
-
-class dummy_instance : public falcosecurity::plugin_instance {
-public:
-	dummy_instance(dummy_plugin &plugin);
-	virtual ~dummy_instance();
-
-	// All of these are from falcosecurity::plugin_instance_iface.
-	ss_plugin_rc open(const char *params) override;
-	void close() override;
-	ss_plugin_rc next(falcosecurity::plugin_event &evt) override;
-
-private:
-	// The plugin that created this instance
-	dummy_plugin &m_plugin;
-
-	// All of these reflect potential internal state for the
-	// instance.
-
-	// Copy of the init params from plugin_open()
-	std::string m_params;
-
-	// The number of events to return before EOF
-	uint64_t m_max_events;
-
-	// A count of events returned. Used to count against m_max_events
-	uint64_t m_counter;
-
-	// A semi-random numeric value, derived from this value and
-	// jitter. This is put in every event as the data property.
-	uint64_t m_sample;
-};
-
-dummy_plugin::dummy_plugin()
-	: m_jitter(10)
+void dummy_plugin::info(falcosecurity::plugin::information& out) const 
 {
-};
-
-dummy_plugin::~dummy_plugin()
-{
-};
-
-
-void dummy_plugin::get_info(falcosecurity::plugin_info &info)
-{
-	info.name = "dummy_c";
-	info.description = "Reference plugin for educational purposes";
-	info.contact = "github.com/falcosecurity/plugins";
-	info.version = "0.2.2";
-	info.event_source = "dummy_c";
-	info.fields = {
-		{FTYPE_UINT64, "dummy.divisible", true, "Return 1 if the value is divisible by the provided divisor, 0 otherwise"},
-		{FTYPE_UINT64, "dummy.value", false, "The sample value in the event"},
-		{FTYPE_STRING, "dummy.strvalue", false, "The sample value in the event, as a string"}
-	};
+    out.name = "dummy_c";
+	out.description = "Reference plugin for educational purposes";
+    out.contact = "https://github.com/falcosecurity/plugins";
+    out.version = "0.2.3";
 }
 
-ss_plugin_rc dummy_plugin::init(const char *config)
+bool dummy_plugin::init(const std::string& config)
 {
-	m_config = config != NULL ? config : "";
-
+    m_config = config;
 	// Config is optional. In this case defaults are used.
 	if(m_config == "" || m_config == "{}")
 	{
-		return SS_PLUGIN_SUCCESS;
+		return true;
 	}
 
 	json obj;
-
 	try {
 		obj = json::parse(m_config);
-	}
-	catch (std::exception &e)
-	{
-		set_last_error(e.what());
-		return SS_PLUGIN_FAILURE;
+	}catch(std::exception e){
+		//set_last_error(e.what());
+		return false;
 	}
 
 	auto it = obj.find("jitter");
-
-	if(it == obj.end())
+	if(it != obj.end())
 	{
-		set_last_error("jitter not defined");
-		return SS_PLUGIN_FAILURE;
+        m_jitter = *it;
 	}
 
-	m_jitter = *it;
-
-	return SS_PLUGIN_SUCCESS;
-}
-
-void dummy_plugin::destroy()
-{
-}
-
-falcosecurity::plugin_instance *dummy_plugin::create_instance(falcosecurity::source_plugin &plugin)
-{
-	return new dummy_instance((dummy_plugin &) plugin);
-
-}
-
-std::string dummy_plugin::event_to_string(const uint8_t *data, uint32_t datalen)
-{
-	// The string representation of an event is a json object with the sample
-	std::string rep = "{\"sample\": ";
-	rep.append((char *) data, datalen);
-	rep += "}";
-
-	return rep;
-}
-
-bool dummy_plugin::extract_str(const ss_plugin_event &evt, const std::string &field, const std::string &arg, std::string &extract_val)
-{
-	if (field == "dummy.strvalue")
+	auto max_events = obj.find("maxEvents");
+	if(max_events != obj.end())
 	{
-		extract_val.assign((char *) evt.data, evt.datalen);
-		return true;
+        m_max_events = *max_events;
 	}
 
-	return false;
+	return true;
 }
 
-bool dummy_plugin::extract_u64(const ss_plugin_event &evt, const std::string &field, const std::string &arg, uint64_t &extract_val)
+void dummy_plugin::last_error(std::string& out) const
 {
-	std::string sample((char *) evt.data, evt.datalen);
-	uint64_t isample = std::stoi(sample);
-
-	if(field == "dummy.divisible")
-	{
-		uint64_t divisor = std::stoi(arg);
-		if ((isample % divisor) == 0)
-		{
-			extract_val = 1;
-		}
-		else
-		{
-			extract_val = 0;
-		}
-
-		return true;
-	}
-	else if (field == "dummy.value")
-	{
-		extract_val = isample;
-
-		return true;
-	}
-
-	return false;
+    out.clear();
 }
 
-uint64_t dummy_plugin::jitter()
+void dummy_plugin::fields(std::vector<falcosecurity::field_extractor::field>& out) const 
 {
-	return m_jitter;
+    falcosecurity::field_extractor::field f;
+    f.name = "dummy.divisible";
+    f.type = FTYPE_UINT64;
+    f.display = "Return 1 if the value is divisible by the provided divisor, 0 otherwise";
+    f.description = "Return 1 if the value is divisible by the provided divisor, 0 otherwise";
+    f.arg.index = true;
+    out.clear();
+    out.push_back(f);
+
+    f.name = "dummy.value";
+    f.type = FTYPE_UINT64;
+    f.display = "The sample value in the event";
+    f.description = "The sample value in the event";
+    out.push_back(f);
+
+    f.name = "dummy.strvalue";
+    f.type = FTYPE_STRING;
+    f.display = "The sample value in the event, as a string";
+    f.description = "The sample value in the event, as a string";
+    out.push_back(f);
 }
 
-dummy_instance::dummy_instance(dummy_plugin &plugin)
-	: m_plugin(plugin)
+bool dummy_plugin::extract(const ss_plugin_event* evt, ss_plugin_extract_field* field) 
 {
+    uint64_t divisor;
+    uint64_t res = 0;
+    switch (field->field_id) {
+        case 0: //dummy.divisible
+             divisor = field->arg_index;
+            if ((m_sample % divisor) == 0)
+            {
+                res = 1;
+            }
+
+            field->res.u64 = &res;
+            field->res_len = 1;
+            return true;
+        case 1: //dummy.value
+            field->res.u64 = (uint64_t*) evt->data;
+            field->res_len = 1;
+            return true;
+        case 2: //dummy.strvalue
+            // The event payload is simply the sample, as a string
+            // XXX check < 10
+            std::string payload = "__"+std::to_string(*evt->data)+"__";
+            const char* payload_ptr = payload.c_str();
+
+            field->res.str = (const char**) &payload_ptr;
+            field->res_len = 1;
+
+            return true;
+    }
+    return false;
 }
 
-dummy_instance::~dummy_instance()
+uint32_t dummy_plugin::id() const 
 {
+    return 999;
 }
 
-ss_plugin_rc dummy_instance::open(const char *params)
+void dummy_plugin::event_source(std::string& out) const 
 {
-	m_params = params;
-
-	// Params are optional. In this case defaults are used.
-	if(m_params == "" || m_params == "{}")
-	{
-		return SS_PLUGIN_SUCCESS;
-	}
-
-	json obj;
-
-	try {
-		obj = json::parse(m_params);
-	}
-	catch (std::exception &e)
-	{
-		std::string errstr = std::string("Params ") + m_params + " could not be parsed: " + e.what();
-		m_plugin.set_last_error(errstr);
-		return SS_PLUGIN_FAILURE;
-	}
-
-	auto start_it = obj.find("start");
-	if(start_it == obj.end())
-	{
-		std::string errstr = std::string("Params ") + m_params + " did not contain start property";
-		m_plugin.set_last_error(errstr);
-		return SS_PLUGIN_FAILURE;
-	}
-
-	auto max_events_it = obj.find("maxEvents");
-	if(max_events_it == obj.end())
-	{
-		std::string errstr = std::string("Params ") + m_params + " did not contain maxEvents property";
-		m_plugin.set_last_error(errstr);
-		return SS_PLUGIN_FAILURE;
-	}
-
-	m_counter = 0;
-	m_max_events = *max_events_it;
-	m_sample = *start_it;
-
-	return SS_PLUGIN_SUCCESS;
+    out = "dummy_c";
 }
 
-void dummy_instance::close()
+std::unique_ptr<falcosecurity::event_sourcer::instance> dummy_plugin::open(const std::string& params) 
 {
+    auto instance = new dummy_instance(m_max_events, m_jitter, &m_sample);
+    return std::unique_ptr<falcosecurity::event_sourcer::instance>(instance);
 }
 
-ss_plugin_rc dummy_instance::next(falcosecurity::plugin_event &evt)
+ss_plugin_rc dummy_instance::next(const falcosecurity::event_sourcer* p, ss_plugin_event* evt) 
 {
-	m_counter++;
+    m_count++;
 
-	if(m_counter > m_max_events)
-	{
-		return SS_PLUGIN_EOF;
-	}
+    if(m_count > m_max_events)
+    {
+        return SS_PLUGIN_EOF;
+    }
 
-	// Increment sample by 1, also add a jitter of [0:jitter]
-	m_sample = m_sample + 1 + (random() % (m_plugin.jitter() + 1));
+    // Increment sample by 1, also add a jitter of [0:jitter]
+    *m_sample = *m_sample + 1 + (random() % (m_jitter + 1));
 
-	// The event payload is simply the sample, as a string
-	std::string payload = std::to_string(m_sample);
+    evt->data = (uint8_t*) m_sample;
+    evt->datalen = sizeof(uint64_t);
 
-	// Note that evtnum is not set, as event numbers are
-	// assigned by the plugin framework.
-	evt.data.assign(payload.begin(), payload.end());
-
-	// Let the plugin framework assign timestamps
-	evt.ts = (uint64_t) -1;
-
-	return SS_PLUGIN_SUCCESS;
+    return SS_PLUGIN_SUCCESS;
 }
-
-// This macro creates the plugin_xxx functions that comprise the
-// source plugin API. It creates dummy_plugin and dummy_instance
-// objects as needed and translates the plugin API calls into the
-// methods in falcosecurity::source_plugin_iface and
-// falcosecurity::plugin_instance_iface.
-GEN_SOURCE_PLUGIN_API_HOOKS(dummy_plugin, dummy_instance)
---
+++ plugins/dummy_c/dummy.h
@@ -0,0 +1,59 @@
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <falcosecurity/sdk.h>
+
+class dummy_instance: public falcosecurity::event_sourcer::instance
+{
+    public:
+        dummy_instance(uint64_t max_events, uint64_t jitter, uint64_t *sample):
+            m_max_events(max_events),
+            m_jitter(jitter),
+            m_sample(sample)
+        {}
+
+        ss_plugin_rc next(const falcosecurity::event_sourcer* p, ss_plugin_event* evt) override;
+
+    private:
+        uint64_t m_count = 0;
+        uint64_t *m_sample;
+        uint64_t m_max_events;
+        uint64_t m_jitter;
+};
+
+class dummy_plugin:
+    public falcosecurity::field_extractor,
+    public falcosecurity::event_sourcer
+{
+    public:
+        void info(falcosecurity::plugin::information&) const override;
+        bool init(const std::string& config) override;
+        void last_error(std::string& out) const override;
+
+        void fields(std::vector<falcosecurity::field_extractor::field>& out) const override;
+        bool extract(const ss_plugin_event* evt, ss_plugin_extract_field* field) override;
+
+        uint32_t id() const;
+        void event_source(std::string& out) const;
+        std::unique_ptr<falcosecurity::event_sourcer::instance> open(const std::string& params) override;
+    private:
+        std::string m_config;
+        uint64_t m_jitter = 10;
+        uint64_t m_max_events = 100;
+        uint64_t m_sample = 1;
+};
