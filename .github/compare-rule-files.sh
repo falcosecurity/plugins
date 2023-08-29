4951da69c1f5974c234b1000355244cfe3b2e862
Jason Dellaluce <jasondellaluce@gmail.com>
2023-08-29 12:56:39 +0000

    fix(ci): copy temp rules file during rules checks
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- .github/compare-rule-files.sh
+++ .github/compare-rule-files.sh
@@ -13,6 +13,11 @@ set -e pipefail
 rm -f $RESULT_FILE
 touch $RESULT_FILE
 
+cur_branch=`git rev-parse HEAD`
+echo Current branch is \"$cur_branch\"
+echo Checking version for rules file \"$RULES_FILE\"...
+cp $RULES_FILE tmp_rule_file.yaml
+
 git checkout tags/$LATEST_TAG
 chmod +x $CHECKER_TOOL
 $CHECKER_TOOL \
