eaa8fb10b94c62b86b96f71eb3555b6d3d2f24cf
Lorenzo Susini <susinilorenzo1@gmail.com>
2023-11-02 16:53:08 +0000

    update(build): create new engine_version_semver string for new plugin rulesfiles  artifact configs
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- build/registry/pkg/oci/requirements.go
+++ build/registry/pkg/oci/requirements.go
@@ -22,7 +22,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/blang/semver"
@@ -65,15 +64,25 @@ func rulesfileRequirement(filePath string) (*oci.ArtifactRequirement, error) {
 	}
 
 	// Split the requirement and parse the version to semVer.
+	// In case the requirement was expressed as a numeric value,
+	// we convert it to semver and treat it as minor version.
 	tokens := strings.Split(fileScanner.Text(), ":")
-	reqVer, err := semver.ParseTolerant(tokens[1])
+	reqVer, err := semver.Parse(tokens[1])
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse to semVer the version requirement %q", tokens[1])
+		reqVer, err = semver.ParseTolerant(tokens[1])
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse requirement %q: expected a numeric value or a valid semver string", tokens[1])
+		}
+		reqVer = semver.Version{
+			Major: 0,
+			Minor: reqVer.Major,
+			Patch: 0,
+		}
 	}
 
 	return &oci.ArtifactRequirement{
 		Name:    common.EngineVersionKey,
-		Version: strconv.FormatUint(reqVer.Major, 10),
+		Version: reqVer.String(),
 	}, nil
 }
 
