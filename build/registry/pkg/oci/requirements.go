8d1932e4f5316efbd99af019015433226333b736
Luca Guerra <luca@guerra.sh>
2024-01-18 16:15:37 +0000

    Revert "update(build): create new engine_version_semver string for new plugin rulesfiles  artifact configs"
    This reverts commit eaa8fb10b94c62b86b96f71eb3555b6d3d2f24cf.
    
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- build/registry/pkg/oci/requirements.go
+++ build/registry/pkg/oci/requirements.go
@@ -22,6 +22,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/blang/semver"
@@ -64,25 +65,15 @@ func rulesfileRequirement(filePath string) (*oci.ArtifactRequirement, error) {
 	}
 
 	// Split the requirement and parse the version to semVer.
-	// In case the requirement was expressed as a numeric value,
-	// we convert it to semver and treat it as minor version.
 	tokens := strings.Split(fileScanner.Text(), ":")
-	reqVer, err := semver.Parse(tokens[1])
+	reqVer, err := semver.ParseTolerant(tokens[1])
 	if err != nil {
-		reqVer, err = semver.ParseTolerant(tokens[1])
-		if err != nil {
-			return nil, fmt.Errorf("unable to parse requirement %q: expected a numeric value or a valid semver string", tokens[1])
-		}
-		reqVer = semver.Version{
-			Major: 0,
-			Minor: reqVer.Major,
-			Patch: 0,
-		}
+		return nil, fmt.Errorf("unable to parse to semVer the version requirement %q", tokens[1])
 	}
 
 	return &oci.ArtifactRequirement{
 		Name:    common.EngineVersionKey,
-		Version: reqVer.String(),
+		Version: strconv.FormatUint(reqVer.Major, 10),
 	}, nil
 }
 
