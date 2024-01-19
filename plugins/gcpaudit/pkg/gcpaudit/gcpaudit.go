466eab065a396a22e7a272596390595a15c17ed4
Luca Guerra <luca@guerra.sh>
2024-01-19 14:18:33 +0000

    update(plugins): gcpaudit-0.3.0
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- plugins/gcpaudit/pkg/gcpaudit/gcpaudit.go
+++ plugins/gcpaudit/pkg/gcpaudit/gcpaudit.go
@@ -30,7 +30,7 @@ const (
 	PluginName               = "gcpaudit"
 	PluginDescription        = "Read GCP Audit Logs"
 	PluginContact            = "github.com/falcosecurity/plugins"
-	PluginVersion            = "0.2.2"
+	PluginVersion            = "0.3.0"
 	PluginEventSource        = "gcp_auditlog"
 )
 
