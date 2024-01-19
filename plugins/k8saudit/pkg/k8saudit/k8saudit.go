091c6bb68066b7e4e60706a43fdfb2b3b5434b1e
Luca Guerra <luca@guerra.sh>
2024-01-19 14:12:21 +0000

    update(plugins): k8saudit-0.7.0
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- plugins/k8saudit/pkg/k8saudit/k8saudit.go
+++ plugins/k8saudit/pkg/k8saudit/k8saudit.go
@@ -54,7 +54,7 @@ func (k *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events and monitor Kubernetes Clusters",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.6.1",
+		Version:     "0.7.0",
 		EventSource: "k8s_audit",
 	}
 }
