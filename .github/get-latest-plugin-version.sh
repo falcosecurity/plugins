83d5b928e5f939e949f294249d3884e5579440ff
Aldo Lacuku <aldo@lacuku.eu>
2024-04-24 14:36:33 +0200

    fix(ci): correctly retrieve plugin version from tag
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- .github/get-latest-plugin-version.sh
+++ .github/get-latest-plugin-version.sh
@@ -5,7 +5,7 @@ PLUGIN=$1
 set +e pipefail
 echo Searching tag with prefix prefix \"${PLUGIN}-\"...
 git fetch --tags origin
-latest_tag=`git describe --match="${PLUGIN}-*.*.*" --exclude="${PLUGIN}-*.*.*-*" --abbrev=0 --tags $(git rev-list --tags="${PLUGIN}-*.*.*" --max-count=1)`
+latest_tag=`git describe --match="${PLUGIN}-[0-9]*" --match="plugins/${PLUGIN}/v*" --abbrev=0 --tags`
 set -e pipefail
 
 latest_ver="0.0.0"
@@ -14,7 +14,11 @@ then
   echo Not previous tag has been found
 else
   echo Most recent tag found is \"$latest_tag\"
-  latest_ver=$(echo $latest_tag | cut -d '-' -f 2-)
+  if [[ "${latest_tag}" == "plugins/${PLUGIN}"* ]]; then
+      latest_ver="${latest_tag##*/v}"
+  else
+      latest_ver="${latest_tag##*-}"
+  fi
 fi
 
 echo Setting plugin version for "${PLUGIN}" to $latest_ver
