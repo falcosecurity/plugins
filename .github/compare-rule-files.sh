4ab3e962389a9989e9b640728d4afaf77f446506
Aldo Lacuku <aldo@lacuku.eu>
2024-04-26 15:48:56 +0200

    feat(CI): extend compare-rule-files to multiple rule files
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- .github/compare-rule-files.sh
+++ .github/compare-rule-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RULES_FILE=$1
+RULES_DIR=$1
 CONFIG_FILE=$2
 PLUGIN_NAME=$3
 RESULT_FILE=$4
@@ -21,8 +21,24 @@ done
 
 cur_branch=`git rev-parse HEAD`
 echo Current branch is \"$cur_branch\"
-echo Checking version for rules file \"$RULES_FILE\"...
-cp $RULES_FILE tmp_rule_file.yaml
+echo Checking version for rules file in dir \"$RULES_DIR\"...
+# Get the rules files and save them.
+# We sort the rules files but first we remove the file extension.
+rules_files=$(ls ${RULES_DIR}/* | while read -r line; do echo "${line%.yaml}"; done | sort)
+# Add the extension to the files.
+# Append the .yaml extension back to the sorted strings
+rules_files=$(echo "${rules_files}" | sed 's/$/.yaml/')
+echo Rule files found: ${rules_files}
+
+# We save the current rules files before going back to the previous
+# version.
+prefix="tmp-"
+for rules_file in ${rules_files}; do
+    new_file="${prefix}$(basename "$rules_file")"
+    echo "Copying rules file ${rules_file} to temporary file ${new_file}"
+    cp "$rules_file" "$new_file"
+    tmp_rules+=" $new_file"
+done
 
 git checkout tags/$LATEST_TAG
 chmod +x $CHECKER_TOOL
@@ -30,13 +46,13 @@ $CHECKER_TOOL \
     compare \
     --falco-image=$FALCO_DOCKER_IMAGE \
     -c $CONFIG_FILE \
-    -l $RULES_FILE \
-    -r tmp_rule_file.yaml \
+    -l ${rules_files} \
+    -r ${tmp_rules} \
     ${extra_flags} \
 1>tmp_res.txt
 git switch --detach $cur_branch
 
-echo '##' $(basename $RULES_FILE) >> $RESULT_FILE
+echo '##' $(basename $RULES_DIR) >> $RESULT_FILE
 echo Comparing \`$cur_branch\` with latest tag \`$LATEST_TAG\` >> $RESULT_FILE
 echo "" >> $RESULT_FILE
 if [ -s tmp_res.txt ]
@@ -47,5 +63,5 @@ else
 fi
 echo "" >> $RESULT_FILE
 
-rm -f tmp_rule_file.yaml
+rm -f ${tmp_rules}
 rm -f tmp_res.txt
