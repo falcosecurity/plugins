fddb668920d2e580fb30655b0cfeede13758fe2d
Luca Guerra <luca@guerra.sh>
2024-05-21 16:47:08 +0200

    chore(plugins/k8saudit): use rc tag
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- plugins/k8saudit/pkg/k8saudit/k8saudit.go
+++ plugins/k8saudit/pkg/k8saudit/k8saudit.go
@@ -54,7 +54,7 @@ func (k *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events and monitor Kubernetes Clusters",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.10.0",
+		Version:     "0.10.0-rc1",
 		EventSource: "k8s_audit",
 	}
 }
