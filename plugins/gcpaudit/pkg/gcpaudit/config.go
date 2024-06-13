77d1d6fbf13fb27e274622ec49e57a5404d39035
Sanja Kosier <sanja.kosier@sysdig.com>
2024-06-13 13:49:24 +0200

    feat(plugins/gcpaudit): introduce optional UseAsync field in gcpaudit plugin
    Signed-off-by: Sanja Kosier <sanja.kosier@sysdig.com>

--- plugins/gcpaudit/pkg/gcpaudit/config.go
+++ plugins/gcpaudit/pkg/gcpaudit/config.go
@@ -36,6 +36,7 @@ type PluginConfig struct {
 	CredentialsFile        string `json:"credentials_file" jsonschema:"title=Credentials File,description=If non-empty overrides the default GCP credentials file (e.g. ~/.config/gcloud/application_default_credentials.json) and env variables such as GOOGLE_APPLICATION_CREDENTIALS (Default: empty),default="`
 	NumGoroutines          int    `json:"num_goroutines" jsonschema:"title=Num Goroutines,description=The number of goroutines that each datastructure along the Receive path will spawn (Default: 10),default=10"`
 	MaxOutstandingMessages int    `json:"max_outstanding_messages" jsonschema:"title=Max Outstanding Messages,description=The maximum number of unprocessed messages (Default: 1000),default=1000"`
+	UseAsync               bool   `json:"useAsync" jsonschema:"title=Use async extraction,description=If true then async extraction optimization is enabled (Default: true),default=true"`
 }
 
 // Reset sets the configuration to its default values
