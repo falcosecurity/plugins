61fda9138621c9ea6efd6032d0272c3d92146f24
Aldo Lacuku <aldo@lacuku.eu>
2024-11-08 11:33:14 +0100

    chore(k8smeta): bump k8smeta version to 0.2.1
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- plugins/k8smeta/src/shared_with_tests_consts.h
+++ plugins/k8smeta/src/shared_with_tests_consts.h
@@ -77,7 +77,7 @@ limitations under the License.
 // Generic plugin consts
 /////////////////////////
 #define PLUGIN_NAME "k8smeta"
-#define PLUGIN_VERSION "0.2.0"
+#define PLUGIN_VERSION "0.2.1"
 #define PLUGIN_DESCRIPTION                                                     \
     "Enrich syscall events with information about the pod that throws them"
 #define PLUGIN_CONTACT "github.com/falcosecurity/plugins"
