11e69492af8c778b719896875086c1f723713b78
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-03-24 16:09:54 +0100

    fix(ebpf): fix timestamp calculation by including boot time
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/krsi/krsi-ebpf/src/shared_maps.rs
+++ plugins/krsi/krsi-ebpf/src/shared_maps.rs
@@ -7,6 +7,9 @@ use krsi_common::EventType;
 // The number of max entries is set, in userspace, to the value of available CPU.
 static AUXILIARY_MAPS: Array<crate::auxmap::AuxiliaryMap> = Array::with_max_entries(0, 0);
 
+#[no_mangle]
+static BOOT_TIME: u64 = 0;
+
 #[map]
 static EVENTS: RingBuf = RingBuf::with_byte_size(128 * 4096, 0); // 128 pages = 256KB
 
@@ -29,5 +32,5 @@ pub fn get_event_num_params(event_type: EventType) -> u8 {
 }
 
 pub fn get_boot_time() -> u64 {
-    0 // TODO: implement
+    unsafe {core::ptr::read_volatile(&BOOT_TIME)}
 }
