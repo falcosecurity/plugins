2d9f21ce490c7bde08a9856b56475ef256193f94
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-03-24 13:02:31 +0100

    fix(ebpf): handle `fd_install` invocations only in open context
    Ensure that `fd_install` invocations are handled only if the current
    thread is trying to open a file: this means hooking at
    `do_sys_openat2`'s entry and exit point to mark the file opening
    procedure as started and finished.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/krsi/krsi-ebpf/src/main.rs
+++ plugins/krsi/krsi-ebpf/src/main.rs
@@ -1,9 +1,25 @@
+//! Open parameter extraction flow:
+//! 1. `fentry:do_sys_openat2` - detect the start of an opening request and annotate it by putting
+//! the pid of the current thread in the `OPEN_PIDS` map
+//! 2. `fexit:security_file_open` - verify that the opening request has been accepted, extract the
+//! file path, put the extracted file path in the auxmap final location and write the association
+//! between the pid of the current thread and the file path length in the `OPEN_PIDS` map. If any of
+//! aforementioned operation fails, ensure the pid is removed from the `OPEN_PIDS` map.
+//! 3. `fentry:fd_install` - verify that an opening request is currently in progress by checking
+//! the presence of an association for the current thread's pid in the `OPEN_PIDS` map, extract the
+//! file path length from the aforementioned association, extract the other relevant parameters for
+//! the opening request, complete the event in the auxiliary map by leveraging the information on
+//! the length of the already-extracted file path and submit the event.
+//! 1. `fexit:do_sys_openat2` - ensure the association for the current thread's pid is removed from
+//! the `OPEN_PIDS` map
+
 #![no_std]
 #![no_main]
 
 use aya_ebpf::cty::{c_int, c_uint};
 use aya_ebpf::helpers::{bpf_probe_read_kernel, bpf_probe_read_kernel_str_bytes};
 use aya_ebpf::macros::{fentry, fexit, kretprobe};
+use aya_ebpf::maps::HashMap;
 use aya_ebpf::programs::{FEntryContext, FExitContext, RetProbeContext};
 use aya_ebpf::{macros::kprobe, programs::ProbeContext, EbpfContext};
 use aya_log_ebpf::info;
@@ -29,26 +45,56 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+#[fentry]
+pub fn do_sys_openat2_e(ctx: FEntryContext) -> u32 {
+    let pid = ctx.pid();
+    const ZERO: u32 = 0;
+    match maps::get_open_pids_map().insert(&pid, &ZERO, 0) {
+        Ok(_) => 0,
+        Err(_) => 1,
+    }
+}
+
 #[fexit]
 pub fn security_file_open(ctx: FExitContext) -> u32 {
     unsafe { try_security_file_open(ctx) }.unwrap_or(1)
 }
 
+const MAX_PATH: u16 = 4096;
+
 unsafe fn try_security_file_open(ctx: FExitContext) -> Result<u32, i64> {
+    let pid = ctx.pid();
+    let open_pids_map = maps::get_open_pids_map();
+    let Some(file_path_len) = open_pids_map.get_ptr_mut(&pid) else {
+        return Ok(0);
+    };
+
     let ret: c_int = unsafe { ctx.arg(1) };
     if ret != 0 {
-        return Ok(0);
+        return remove_open_pid(open_pids_map, pid);
     }
 
-    let auxmap = maps::get_auxiliary_map().ok_or(1_i64)?;
+    let Some(auxmap) = maps::get_auxiliary_map() else {
+        return remove_open_pid(open_pids_map, pid);
+    };
+
     auxmap.preload_event_header(EventType::FdInstall);
     auxmap.store_param(0_u64);
     let file: *const vmlinux::file = ctx.arg(0);
     let path_ptr = &(*file).f_path as *const vmlinux::path;
-    let written_bytes = auxmap.store_path_param(path_ptr, MAX_PATH)? as u32;
-    let pid = ctx.pid();
-    maps::get_open_pids_map().insert(&pid, &written_bytes, 0)?;
+    let Ok(written_bytes) = auxmap.store_path_param(path_ptr, MAX_PATH) else {
+        return remove_open_pid(open_pids_map, pid);
+    };
+
+    *file_path_len = written_bytes as u32;
     Ok(0)
+}
+
+fn remove_open_pid(open_pids_map: &HashMap<u32, u32>, pid: u32) -> Result<u32, i64> {
+    match open_pids_map.remove(&pid) {
+        Ok(_) => Ok(0),
+        Err(_) => Err(1),
+    }
 }
 
 #[fentry]
@@ -63,19 +109,7 @@ unsafe fn try_fd_install(ctx: FEntryContext) -> Result<u32, i64> {
     };
     let file_path_len = file_path_len as u16;
 
-    let _ = try_fd_install_extract_params(ctx, file_path_len);
-    maps::get_open_pids_map().remove(&pid)?;
-
-    Ok(0)
-}
-
-const MAX_PATH: u16 = 4096;
-
-unsafe fn try_fd_install_extract_params(
-    ctx: FEntryContext,
-    file_path_len: u16,
-) -> Result<u32, i64> {
-    let auxmap = maps::get_auxiliary_map().ok_or(1_i64)?;
+    let auxmap = maps::get_auxiliary_map().ok_or(1)?;
     auxmap.preload_event_header(EventType::FdInstall);
 
     // Parameter 1: fd.
@@ -138,5 +172,15 @@ unsafe fn try_fd_install_extract_params(
             ino
         );
     }
+
     Ok(0)
+}
+
+#[fexit]
+pub fn do_sys_openat2_x(ctx: FExitContext) -> u32 {
+    let pid = ctx.pid();
+    match maps::get_open_pids_map().remove(&pid) {
+        Ok(_) => 0,
+        Err(_) => 1,
+    }
 }
