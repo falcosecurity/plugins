99c7f3ddb85c1fd562f8c7bb034dd1c261245b73
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-03-25 10:37:34 +0100

    refactor(ebpf): replace `tid` with `pid`
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/krsi/krsi-ebpf/src/connect/maps.rs
+++ plugins/krsi/krsi-ebpf/src/connect/maps.rs
@@ -2,7 +2,7 @@ use aya_ebpf::macros::map;
 use aya_ebpf::maps::HashMap;
 
 #[map]
-static SOCK_TIDS: HashMap<u32, u32> = HashMap::with_max_entries(32768, 0);
+static SOCK_PIDS: HashMap<u32, u32> = HashMap::with_max_entries(32768, 0);
 
 #[map]
 static SOCK_PTRS: HashMap<u32, usize> = HashMap::with_max_entries(32768, 0);
@@ -10,8 +10,8 @@ static SOCK_PTRS: HashMap<u32, usize> = HashMap::with_max_entries(32768, 0);
 #[map]
 static SOCK_RES: HashMap<SockPtrTgid, u32> = HashMap::with_max_entries(32768, 0);
 
-pub fn get_sock_tids_map() -> &'static HashMap<u32, u32> {
-    &SOCK_TIDS
+pub fn get_sock_pids_map() -> &'static HashMap<u32, u32> {
+    &SOCK_PIDS
 }
 
 pub fn get_sock_ptrs_map() -> &'static HashMap<u32, usize> {
