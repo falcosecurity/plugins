895a38df96aee583cc2c40e1be23db8cfabdf1ec
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-03-25 10:37:34 +0100

    refactor(ebpf): replace `tid` with `pid`
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/krsi/krsi-ebpf/src/connect.rs
+++ plugins/krsi/krsi-ebpf/src/connect.rs
@@ -8,19 +8,19 @@
 //!
 //! Flow for extracting parameters upon socket creation procedures:
 //! 1. `fentry:__sys_socket` - detect the start of a socket creation procedure and annotate it by
-//! putting the current thread's tid in the `SOCK_TIDS` map
+//! putting the current task's pid in the `SOCK_PIDS` map
 //! 2. `fexit:sock_alloc_file` - verify that socket creation has been accepted and, if the
 //! `sock_alloc_file` operation was successful, extract the socket struct pointer, write the
-//! association between the current thread's tid and the extracted socket struct pointer in the
-//! `SOCK_PTRS` map. In any case, remove the association for the current thread's tid from the
-//! `SOCK_TIDS` map.
+//! association between the current task's pid and the extracted socket struct pointer in the
+//! `SOCK_PTRS` map. In any case, remove the association for the current task's pid from the
+//! `SOCK_PIDS` map.
 //! 3. `fentry:fd_install` - verify that a socket creation request is currently in progress by
-//! checking the presence of an association for the current thread's tid in the `SOCK_PTRS` map,
+//! checking the presence of an association for the current task's pid in the `SOCK_PTRS` map,
 //! extract the socket struct pointer from the aforementioned association and write the association
 //! (sock_struct_ptr, tgid) -> fd in the `SOCK_RES` map. In any case, remove the association for the
-//! current thread's tid from `SOCK_PTRS`
-//! 4. `fexit:__sys_socket` - ensure the associations for the current thread's tid are removed from
-//! the `SOCK_TIDS` and `SOCK_PTRS` maps
+//! current task's pid from `SOCK_PTRS`
+//! 4. `fexit:__sys_socket` - ensure the associations for the current task's pid are removed from
+//! the `SOCK_PIDS` and `SOCK_PTRS` maps
 
 use crate::connect::maps::SockPtrTgid;
 use crate::vmlinux;
@@ -36,9 +36,9 @@ mod maps;
 
 #[fentry]
 fn __sys_socket_e(ctx: FEntryContext) -> u32 {
-    let tid = ctx.pid();
+    let pid = ctx.pid();
     const ZERO: u32 = 0;
-    try_insert_entry(maps::get_sock_tids_map(), &tid, &ZERO).unwrap_or(1)
+    try_insert_entry(maps::get_sock_pids_map(), &pid, &ZERO).unwrap_or(1)
 }
 
 #[fexit]
@@ -47,21 +47,21 @@ fn sock_alloc_file(ctx: FExitContext) -> u32 {
 }
 
 fn try_sock_alloc_file(ctx: FExitContext) -> Result<u32, i64> {
-    let tid = ctx.pid();
-    let sock_tids_map = maps::get_sock_tids_map();
-    if unsafe { sock_tids_map.get(&tid) }.is_none() {
+    let pid = ctx.pid();
+    let sock_pids_map = maps::get_sock_pids_map();
+    if unsafe { sock_pids_map.get(&pid) }.is_none() {
         return Ok(0);
     }
 
     let file_ptr: *const vmlinux::file = unsafe { ctx.arg(3) };
     // TODO: verify that the following is the correct check condition
     if is_err(file_ptr) {
-        return try_remove_entry(sock_tids_map, &tid);
+        return try_remove_entry(sock_pids_map, &pid);
     }
 
     let sock_ptr: usize = unsafe { ctx.arg(0) };
-    let res = try_insert_entry(maps::get_sock_ptrs_map(), &tid, &sock_ptr);
-    try_remove_entry(sock_tids_map, &tid).and(res)
+    let res = try_insert_entry(maps::get_sock_ptrs_map(), &pid, &sock_ptr);
+    try_remove_entry(sock_pids_map, &pid).and(res)
 }
 
 fn is_err(file_ptr: *const vmlinux::file) -> bool {
@@ -72,9 +72,9 @@ fn is_err(file_ptr: *const vmlinux::file) -> bool {
 }
 
 pub fn try_fd_install(ctx: &FEntryContext) -> Result<u32, i64> {
-    let tid = ctx.pid();
+    let pid = ctx.pid();
     let sock_ptrs_map = maps::get_sock_ptrs_map();
-    let Some(&sock_ptr) = (unsafe { sock_ptrs_map.get(&tid) }) else {
+    let Some(&sock_ptr) = (unsafe { sock_ptrs_map.get(&pid) }) else {
         return Ok(0);
     };
 
@@ -87,14 +87,14 @@ pub fn try_fd_install(ctx: &FEntryContext) -> Result<u32, i64> {
         ctx,
         "[fd_install][socket]: new association (sock_ptr={}, tgid={}) -> fd={}", sock_ptr, tgid, fd
     );
-    try_remove_entry(sock_ptrs_map, &tid).and(res)
+    try_remove_entry(sock_ptrs_map, &pid).and(res)
 }
 
 #[fexit]
 fn __sys_socket_x(ctx: FExitContext) -> u32 {
-    let tid = ctx.pid();
-    let res1 = try_remove_entry(maps::get_sock_tids_map(), &tid);
-    let res2 = try_remove_entry(maps::get_sock_ptrs_map(), &tid);
+    let pid = ctx.pid();
+    let res1 = try_remove_entry(maps::get_sock_pids_map(), &pid);
+    let res2 = try_remove_entry(maps::get_sock_ptrs_map(), &pid);
     res1.and(res2).unwrap_or(1)
 }
 
