b9a0d7f200d365652e1c272bb721f2de9dfd7131
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-04-04 14:14:33 +0200

    refactor(ebpf): align symlinkat operation map name
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/krsi/krsi-ebpf/src/operations/symlinkat/maps.rs
+++ plugins/krsi/krsi-ebpf/src/operations/symlinkat/maps.rs
@@ -2,8 +2,8 @@ use aya_ebpf::macros::map;
 use aya_ebpf::maps::HashMap;
 
 #[map]
-static IOU_PIDS: HashMap<u32, u32> = HashMap::with_max_entries(32768, 0);
+static SYMLINKAT_IOU_PIDS: HashMap<u32, u32> = HashMap::with_max_entries(32768, 0);
 
 pub fn get_iou_pids_map() -> &'static HashMap<u32, u32> {
-    &IOU_PIDS
+    &SYMLINKAT_IOU_PIDS
 }
