a2af2f414a358a200299007a517d6ea8a1660d98
Leonardo Grasso <me@leonardograsso.com>
2025-05-12 15:46:15 +0200

    update(plugins/k8saudit-eks): bump to v0.8.0
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- plugins/k8saudit-eks/pkg/k8sauditeks/k8sauditeks.go
+++ plugins/k8saudit-eks/pkg/k8sauditeks/k8sauditeks.go
@@ -63,7 +63,7 @@ func (k *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events for EKS from Cloudwatch Logs",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.7.0",
+		Version:     "0.8.0",
 		EventSource: "k8s_audit",
 	}
 }
