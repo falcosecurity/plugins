a31f69b8f2a65d5e35eecb5711d3796a335a2cf4
Leonardo Grasso <me@leonardograsso.com>
2025-09-24 17:03:06 +0200

    update(plugins/kafka): bump to v0.1.5
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- plugins/kafka/pkg/kafka/kafka.go
+++ plugins/kafka/pkg/kafka/kafka.go
@@ -36,7 +36,7 @@ const (
 	PluginName               = "kafka"
 	PluginDescription        = "Read events from Kafka topics into Falco"
 	PluginContact            = "github.com/falcosecurity/plugins"
-	PluginVersion            = "0.1.4"
+	PluginVersion            = "0.1.5"
 	PluginEventSource        = "kafka"
 )
 
