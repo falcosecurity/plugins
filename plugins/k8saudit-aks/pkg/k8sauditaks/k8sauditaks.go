a6a1feae832da6fa12df364bdb7c5e48308cb3a0
Leonardo Grasso <me@leonardograsso.com>
2025-10-06 17:44:31 +0200

    chore(plugins/k8saudit-aks): bump to v0.4.0
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- plugins/k8saudit-aks/pkg/k8sauditaks/k8sauditaks.go
+++ plugins/k8saudit-aks/pkg/k8sauditaks/k8sauditaks.go
@@ -60,7 +60,7 @@ func (p *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events for AKS from EventHub and use blob storage as checkpoint store",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.3.0",
+		Version:     "0.4.0",
 		EventSource: "k8s_audit",
 	}
 }
