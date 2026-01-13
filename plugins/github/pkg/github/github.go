63cbb78151150a4a0c9312faf9b9051334332668
Roberto Scolaro <roberto.scolaro21@gmail.com>
2026-01-13 17:23:03 +0000

    chore(github): bump version to 0.10.0
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- plugins/github/pkg/github/github.go
+++ plugins/github/pkg/github/github.go
@@ -39,7 +39,7 @@ const (
 	PluginName                = "github"
 	PluginDescription         = "Reads github webhook events, by listening on a socket or by reading events from disk"
 	PluginContact             = "github.com/falcosecurity/plugins"
-	PluginVersion             = "0.9.1"
+	PluginVersion             = "0.10.0"
 	PluginEventSource         = "github"
 	ExtractEventSource        = "github"
 )
