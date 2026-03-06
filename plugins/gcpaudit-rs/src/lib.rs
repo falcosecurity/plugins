c050a2cfd8b1bdc783ee45f1fd93fa81ef486531
Gerald Combs <gerald@wireshark.org>
2026-03-05 16:44:01 -0800

    fix(plugins/gcpaudit-rs): First successful build
    Signed-off-by: Gerald Combs <gerald@wireshark.org>

--- plugins/gcpaudit-rs/src/lib.rs
+++ plugins/gcpaudit-rs/src/lib.rs
@@ -15,19 +15,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 
 use anyhow::{anyhow, Result};
-// use falco_plugin::anyhow::Error;
 use config::PluginConfig;
-// use extract::FieldExtractor;
-use falco_plugin::base::{Plugin};
-// use falco_plugin::source::{EventBatch, EventInput, SourcePlugin, SourcePluginInstance};
+use falco_plugin::base::{Json, Plugin};
+use falco_plugin::event::{PluginEvent, events::Event};
 use falco_plugin::source::{EventInput, SourcePlugin};
 use falco_plugin::tables::TablesInput;
 use falco_plugin::{extract_plugin, plugin, source_plugin};
-// use schemars::schema_for;
-// use serde_json::Value;
 use source::GcpAuditInstance;
 
 pub mod config;
@@ -49,21 +45,22 @@ impl Plugin for GcpAuditPlugin {
     const PLUGIN_VERSION: &'static CStr = c"0.1.0";
     const DESCRIPTION: &'static CStr = c"Read GCP Audit Logs";
     const CONTACT: &'static CStr = c"github.com/falcosecurity/plugins";
-    type ConfigType = PluginConfig;
+    type ConfigType = Json<PluginConfig>;
 
     fn new(_input: Option<&TablesInput>, config: Self::ConfigType) -> Result<Self> {
-        Ok(GcpAuditPlugin { config })
+        Ok(GcpAuditPlugin { config: config.0 })
     }
 
     fn set_config(&mut self, config: Self::ConfigType) -> Result<()> {
-        self.config = config;
+        self.config = config.0;
         Ok(())
     }
 }
 
 impl SourcePlugin for GcpAuditPlugin {
     type Instance = GcpAuditInstance;
-    const EVENT_SOURCES: &'static [&'static str] = &["gcpaudit"];
+    type Event<'a> = Event<PluginEvent<&'a [u8]>>;
+    const EVENT_SOURCE: &'static CStr = c"gcpaudit";
     const PLUGIN_ID: u32 = 12;
 
     fn open(&mut self, params: Option<&str>) -> Result<Self::Instance> {
@@ -74,20 +71,11 @@ impl SourcePlugin for GcpAuditPlugin {
         GcpAuditInstance::new(self.config.clone(), subscription_id)
     }
 
-    fn event_to_string(&mut self, event: &EventInput) -> Result<String> {
-        Ok(String::from_utf8_lossy(event.event).to_string())
+    fn event_to_string(&mut self, event: &EventInput<Self::Event<'_>>) -> Result<CString> {
+        let evt = event.event()?;
+        Ok(CString::new(evt.params.event_data.to_vec())?)
     }
 }
-
-// impl ExtractPlugin for GcpAuditPlugin {
-//     // const EVENT_SOURCES: &'static [&'static str] = &["gcpaudit"];
-//     // const EXTRACT_FIELDS: &'static [&'static str] = FieldExtractor::FIELD_NAMES;
-//     // type ExtractContext = FieldExtractor;
-
-//     fn extract_context(&self) -> Self::ExtractContext {
-//         FieldExtractor::new()
-//     }
-// }
 
 plugin!{GcpAuditPlugin}
 source_plugin!(GcpAuditPlugin);
