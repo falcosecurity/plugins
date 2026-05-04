409744ee18ac4f6731634b9410a08120442627f1
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-05-04 15:27:15 +0200

    chore(plugins/k8saudit-aks): bump version to `0.5.0`
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- plugins/k8saudit-aks/pkg/k8sauditaks/k8sauditaks.go
+++ plugins/k8saudit-aks/pkg/k8sauditaks/k8sauditaks.go
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 /*
-Copyright (C) 2025 The Falco Authors.
+Copyright (C) 2026 The Falco Authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package k8sauditaks
 
 import (
@@ -60,7 +64,7 @@ func (p *Plugin) Info() *plugins.Info {
 		Name:        pluginName,
 		Description: "Read Kubernetes Audit Events for AKS from EventHub and use blob storage as checkpoint store",
 		Contact:     "github.com/falcosecurity/plugins",
-		Version:     "0.4.0",
+		Version:     "0.5.0",
 		EventSource: "k8s_audit",
 	}
 }
